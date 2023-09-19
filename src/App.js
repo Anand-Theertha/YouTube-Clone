71c5bf35ff3e61bc6d31dfd2552c1246819c8a59
theerthaAnand <anand.theertha@stride.ai>
2023-09-19 22:10:28 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Box } from "@mui/material";
+import Feed from "./components/Feed";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from "./components/VideoDetail";
+import Navbar from "./components/Navbar";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#ff1f1f" }}>
+      <Navbar />   
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} /> 
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>HHH</div>
+  )
+}
+
+export default Navbar
