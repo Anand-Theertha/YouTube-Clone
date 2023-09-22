084c258b6d197e14184135648fd11a976cd62b42
theerthaAnand <anand.theertha@stride.ai>
2023-09-22 22:37:52 +0530

    Base Sidebar component setup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => (
   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
     <Box
       sx={{
-        height: { sx: "auto", md: "92vh" },
+        height: { sx: "auto", md: "94vh" },
         borderRight: "1px solid #3d3d3d",
         px: { sx: 0, md: 2 },
       }}
     >
-      Sidebar 
+      <Sidebar /> 
       <Typography className="copyright" variant="body2" sx={{mt: 1.5 , color: '#fff'}}> Copyright 2023 FioMedia  </Typography>
     </Box>
   </Stack>
