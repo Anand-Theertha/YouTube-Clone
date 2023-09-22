8900de6a33720a4c469b708ef15b835bd4b12287
theerthaAnand <anand.theertha@stride.ai>
2023-09-23 00:51:03 +0530

    API call and integrate

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,31 +2,53 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
 import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
+const Feed = () => {
 
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setvideos] = useState([]);
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "94vh" },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <Sidebar /> 
-      <Typography className="copyright" variant="body2" sx={{mt: 1.5 , color: '#fff'}}> Copyright 2023 FioMedia  </Typography>
-    </Box>
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => {setvideos(data.items)})
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "94vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          {" "}
+          Copyright 2023 FioMedia{" "}
+        </Typography>
+      </Box>
 
       {/* VIDEOS  */}
-    <Box p={2} sx={{ overflowY : 'auto' , height: '90vh', flex: 2}}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
-        New <span style={{ color : '#f31503'}}>videos</span>
-      </Typography>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
 
-      <Videos videos={[]}></Videos>
-    </Box>
-  </Stack>
-);
+        <Videos videos={videos}></Videos>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+const VideoCard = () => {
+  return <Stack sx={{ textColor: "white" }}>VideoCard</Stack>;
+};
+
+export default VideoCard;
