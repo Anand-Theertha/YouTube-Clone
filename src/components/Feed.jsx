380db6179f03c056438c16e6e7b5fb066f329e56
theerthaAnand <anand.theertha@stride.ai>
2023-09-22 22:45:58 +0530

    Base Feed component setup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+
+
 
 const Feed = () => (
   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -13,6 +16,15 @@ const Feed = () => (
     >
       <Sidebar /> 
       <Typography className="copyright" variant="body2" sx={{mt: 1.5 , color: '#fff'}}> Copyright 2023 FioMedia  </Typography>
+    </Box>
+
+      {/* VIDEOS  */}
+    <Box p={2} sx={{ overflowY : 'auto' , height: '90vh', flex: 2}}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+        New <span style={{ color : '#f31503'}}>videos</span>
+      </Typography>
+
+      <Videos videos={[]}></Videos>
     </Box>
   </Stack>
 );
